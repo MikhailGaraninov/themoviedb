6ba188c0bdcd6467d7ec4645831afab507340f63
MikhailGaraninov <garaninovmkhail@gmail.com>
2022-06-01 08:58:10 +0300

    deleted onGeneralRoute 31.05.2022

--- lib/main.dart
+++ lib/main.dart
@@ -7,21 +7,29 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-
- @override 
- Widget build(BuildContext context) {
-   return MaterialApp(
-     title: "themoviedb",
-     theme: ThemeData(
-       appBarTheme: AppBarTheme(
-         backgroundColor: const Color.fromRGBO(3, 37, 65, 1)),
-       primarySwatch: Colors.blue,
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "themoviedb",
+      theme: ThemeData(
+        appBarTheme:
+            AppBarTheme(backgroundColor: const Color.fromRGBO(3, 37, 65, 1)),
+        primarySwatch: Colors.blue,
       ),
       routes: {
-      '/auth':(context) => AuthWidget(),
-      '/main_screen':(context) => MainScreenWidget(),
+        '/auth': (context) => AuthWidget(),
+        '/main_screen': (context) => MainScreenWidget(),
       },
-    initialRoute: '/auth',
-   );
- }
+      initialRoute: '/auth',
+     /*  onGenerateRoute: (RouteSettings settings) {
+        return MaterialPageRoute<void>(builder: (context) {
+          return Scaffold(
+            body: Center( 
+              child: Text("Произошла ошибка навигации"),
+            ),
+          );
+        });
+      }, */
+    );
+  }
 }
